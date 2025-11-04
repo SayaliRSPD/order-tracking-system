8213a5424ca42159d12b620520b7625c174c85a8
Sayali <duragkarsayali0897@gmail.com>
2025-11-03 22:06:50 -0800

    Changes in orderitem repository.

--- order-tracking/src/main/java/com/walmart/order_tracking/repo/OrderItemRepository.java
+++ order-tracking/src/main/java/com/walmart/order_tracking/repo/OrderItemRepository.java
@@ -3,4 +3,5 @@ package com.walmart.order_tracking.repo;
 import com.walmart.order_tracking.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
